3be755a7e01f6acd35318d804fe9d28c57120067
Yash Trivedi <50360904+yash-trivedi-dev@users.noreply.github.com>
2020-04-27 22:47:34 +0530

    Initial commit

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    public enum motionDirection {Spin, Horizontal, Vertical};
+    public motionDirection motionState = motionDirection.Horizontal;
+    public float SpinSpeed = 180.0f;
+    public float motionMagnitude = 0.1f;
+
+    // Update is called once per fram
+    void Update()
+    {
+        switch(motionState) {
+            case motionDirection.Spin:
+                
+                // rotate around the up axis to the gameObject
+                gameObject.transform.Rotate(Vector3.up * SpinSpeed * Time.deltaTime);
+                break;
+
+            case motionDirection.Horizontal:
+                // move right and left over time
+                gameObject.transform.Translate( Vector3.right * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude );
+                break;
+
+            case motionDirection.Vertical:
+                // move up and down over time
+                gameObject.transform.Translate( Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude );
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartGame : MonoBehaviour
+{
+    // respond on collisions
+	void OnCollisionEnter(Collision newCollision)
+	{
+		// only do stuff if hit by a projectile
+		if (newCollision.gameObject.tag == "Projectile") {
+			// call the NextLevel function in the game manager
+			GameManager.gm.RestartGame();
+		}
+	}
+}
